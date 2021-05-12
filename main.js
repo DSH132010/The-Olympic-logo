e43ea05e775ab213c298168c339688cb1fcdecdc
DSH132010 <80533223+DSH132010@users.noreply.github.com>
2021-05-12 16:38:10 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,50 @@
+canvas = document.getElementById("logo");
+ctx = canvas.getContext("2d");
+
+
+color = "blue";
+ctx.beginPath();
+ctx.strokeStyle = color;
+ctx.lineWidth = 5;
+ctx.arc(230, 210, 40, 0, 2 * Math.PI);
+ctx.stroke();
+
+
+
+
+color = "black"
+ctx.beginPath();
+ctx.strokeStyle = color;
+ctx.lineWidth = 5;
+ctx.arc(300, 210, 40, 0, 2 * Math.PI);
+ctx.stroke();
+
+
+
+
+color = "red"
+ctx.beginPath();
+ctx.strokeStyle = color;
+ctx.lineWidth = 5;
+ctx.arc(370, 205, 40, 0, 2 * Math.PI);
+ctx.stroke();
+
+
+
+
+color = "gold"
+ctx.beginPath();
+ctx.strokeStyle = color;
+ctx.lineWidth = 5;
+ctx.arc(260, 270, 40, 0, 2 * Math.PI);
+ctx.stroke();
+
+
+
+
+color = "green"
+ctx.beginPath();
+ctx.strokeStyle = color;
+ctx.lineWidth = 5;
+ctx.arc(330, 270, 40, 0, 2 * Math.PI);
+ctx.stroke();
